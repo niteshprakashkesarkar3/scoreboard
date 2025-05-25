bc3cde1a99d12e983d205b54d43835ff590088d4
niteshprakashkesarkar3 <niteshprakashkesarkar3@gmail.com>
2025-05-25 16:15:53 +0530

    Updated dashboard.component.ts

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -105,7 +105,7 @@ import { Team } from '../../models/team.model';
       text-decoration: none;
       font-weight: 500;
     }
-
+ 
     .cards-scroll-container {
       margin: 0 -1.5rem;
       padding: 0 1.5rem;
