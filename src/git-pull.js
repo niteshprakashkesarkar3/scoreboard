e932a3852ed11bd2376b863dcd70b27804512672
niteshprakashkesarkar3 <niteshprakashkesarkar3@gmail.com>
2025-05-25 16:35:21 +0530

    Updated package-lock.json

---
+++ src/git-pull.js
@@ -0,0 +1,29 @@
+import git from 'isomorphic-git';
+import http from 'isomorphic-git/http/web';
+import { promises as fs } from 'fs';
+
+async function pullFromGitHub() {
+  try {
+    await git.init({ fs, dir: '.' });
+    await git.addRemote({
+      fs,
+      dir: '.',
+      remote: 'origin',
+      url: 'YOUR_GITHUB_REPO_URL' // Replace with your GitHub repo URL
+    });
+    
+    await git.pull({
+      fs,
+      http,
+      dir: '.',
+      ref: 'main',
+      remote: 'origin',
+    });
+    
+    console.log('Successfully pulled latest changes');
+  } catch (error) {
+    console.error('Error pulling from GitHub:', error);
+  }
+}
+
+pullFromGitHub();
