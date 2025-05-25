859237f48f9bf547dd0242b31093395a3a174412
niteshprakashkesarkar3 <niteshprakashkesarkar3@gmail.com>
2025-05-25 13:49:08 +0530

    Updated match.model.ts

--- src/app/models/match.model.ts
+++ src/app/models/match.model.ts
@@ -2,7 +2,7 @@ export interface Match {
   id: string;
   team1_id: string;
   team2_id: string;
-  tournament_id: string;
+  tournament_id: string; 
   stadium_id: string;
   scheduled_at: Date; 
 }
