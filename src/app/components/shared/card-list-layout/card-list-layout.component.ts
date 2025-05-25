1cedd733a56fd12754a62dc6d448a279906d00de
niteshprakashkesarkar3 <niteshprakashkesarkar3@gmail.com>
2025-05-25 19:41:24 +0530

    Fix Output and EventEmitter imports in CardListLayoutComponent

--- src/app/components/shared/card-list-layout/card-list-layout.component.ts
+++ src/app/components/shared/card-list-layout/card-list-layout.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from '../button/button.component';
 
